3f2e4deedf6c2494612d2ffcd920e6b85d38301e
Erdinc YASAN <erdincyasan@outlook.com>
2026-09-10 23:01:07 +0300

    fix: resolve the operation log from the scope again

--- _02_Lifetimes/Program.cs
+++ _02_Lifetimes/Program.cs
@@ -16,7 +16,7 @@ while (true)
     using var scope = provider.CreateScope();
 
     var translator = scope.ServiceProvider.GetRequiredService<Translator>();
-    var log = provider.GetRequiredService<OperationLogger>();
+    var log = scope.ServiceProvider.GetRequiredService<OperationLogger>();
 
     if (translator.Language.Length == 0)
     {
